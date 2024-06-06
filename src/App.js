4efb2348e653b497b978f69e66374081893c8c40
KhaledShehataA <khaled.ah.shehata@gmail.com>
2024-06-06 14:01:41 +0200

    Feat: the cell component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { useState } from 'react';
+import Cell from './Components/Cell';
+
 const App = () => {
-	return <div></div>;
+	const [Cells, setCells] = useState(['', '', '', '', '', '', '', '', '']);
+	return (
+		<div className='app'>
+			<div className='gameboard'>
+				{Cells.map((cell, index) => (
+					<Cell key={index} id={index} cell={cell} />
+				))}
+			</div>
+			<p></p>
+		</div>
+	);
 };
 
 export default App;
---
+++ src/Components/Cell.js
@@ -0,0 +1,5 @@
+const Cell = ({ id, cell }) => {
+	return <div className='square' id={id}></div>;
+};
+
+export default Cell;
